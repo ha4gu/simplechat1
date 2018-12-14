c53e681249f7f530b2be0be492c21285aee634c0
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-12-14 12:22:15 +0900

    add post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'chat',     to: 'posts#index'
+  get 'chat/:id', to: 'posts#show'
+
   root 'static#top'
-  get  '/about', to: 'static#about'
+  get  'about', to: 'static#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
