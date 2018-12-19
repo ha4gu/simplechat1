53bc790ee381a46a9d324bdee80029d4f2a9ed58
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-12-19 14:12:35 +0900

    show time in JST

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   validates :message, {presence: true, length: {maximum: 800}}
   validates :author,  {presence: true}
+
+  def jsttime
+    self.created_at.in_time_zone('Asia/Tokyo').strftime("%Y-%m-%d %H:%M")
+  end
+
 end
