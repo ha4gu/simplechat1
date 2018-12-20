b11dd451e31c9764161041a64ac3321f34169b53
ha4gu <9391978+ha4gu@users.noreply.github.com>
2018-12-20 17:06:13 +0900

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(
-  name:  "ha4gu",
-  email: "ha4gu@example.com",
+  name:  "Test User 1",
+  email: "user1@example.com",
   image_name: "iconfinder_12_avatar_2754577.png",
   password: "foobar",
 )
 
 User.create(
-  name:  "Hoge",
-  email: "hoge@example.com",
+  name:  "Test User 2",
+  email: "user2@example.com",
   image_name: "iconfinder_3_avatar_2754579.png",
   password: "foobar",
 )
 
 User.create(
-  name:  "Fuga",
-  email: "fuga@example.com",
+  name:  "Test User 3",
+  email: "user3@example.com",
   image_name: "iconfinder_9_avatar_2754584.png",
   password: "foobar",
 )
